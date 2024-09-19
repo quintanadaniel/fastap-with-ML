3cd56b465e64c5ad716b3a4602cc0da3abdd0b73
Daniel Quintana <quintana.danielarmando@gmail.com>
2024-09-19 22:14:05 +0100

    created datadog client files

---
+++ utils/datadog_client.py
@@ -0,0 +1,23 @@
+import os
+
+from datadog import initialize, statsd
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class DatadogClient:
+    def __init__(self):
+        self.api_key = os.getenv("API_KEY")
+        self.app_key = os.getenv("APP_KEY")
+
+    def client_initialize(self):
+        options = {
+            "api_key": self.api_key,
+            "app_key": self.app_key
+        }
+        return initialize(**options)
+
+    def send_metrics_incremental(self, name: str):
+        return statsd.increment(name, 1)
