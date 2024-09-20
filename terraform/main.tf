c9f009c5891b7666989448d15e8d3496f292b7c3
Daniel Quintana <quintana.danielarmando@gmail.com>
2024-09-20 21:15:38 +0100

    modified terraform file correted to get first the subnets using the vpc ids

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+data "aws_subnet_ids" "eks_subnets" {
+  vpc_id = "vpc-05d27574bcd23daaf"
+}
+
 resource "aws_eks_cluster" "cluster_img_det" {
   name     = "cluster_im_det"
   role_arn = "arn:aws:iam::262520292671:role/kube_role"
   vpc_config {
-    subnet_ids = ["subnet-04b246e72d57e052c", "subnet-0341e161613eed3f0"]
+    subnet_ids = data.aws_subnet_ids.eks_subnets.ids #["subnet-04b246e72d57e052c", "subnet-0341e161613eed3f0"]
   }
 }
 
@@ -14,7 +18,7 @@ resource "aws_eks_node_group" "imagen_detector_ng" {
   cluster_name    = aws_eks_cluster.cluster_img_det.name
   node_group_name = "imagen_detector_node_group"
   node_role_arn   = "arn:aws:iam::262520292671:role/kube_role"
-  subnet_ids      = ["subnet-04b246e72d57e052c", "subnet-0341e161613eed3f0"]
+  subnet_ids      = data.aws_subnet_ids.eks_subnets.ids #["subnet-04b246e72d57e052c", "subnet-0341e161613eed3f0"]
 
   scaling_config {
     desired_size = 3
